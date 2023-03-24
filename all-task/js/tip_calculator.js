5ee1d9bf87df8f839c4e17d30a451902cf9e581a
vm9036 <vishalmakwana9714@gmail.com>
2023-03-24 14:20:43 +0530

    update index

--- all-task/js/tip_calculator.js
+++ all-task/js/tip_calculator.js
@@ -1,6 +1,17 @@
 function tip_calculator() {
 
-    var bill_amount = document.getElementById('amount').value = 50;
+    let totalbillAmount = 0;
+
+    let bill_amount = Number(document.getElementById("amount").value);
+    let interestAmount = Number(document.getElementById("tipPercentage").value);
+
+    let interest = (bill_amount * interestAmount) / 100;
+    document.getElementById("tip_amount").value = interest;
+
+    totalbillAmount = bill_amount + interest;
+    document.getElementById("totalBill").value = totalbillAmount;
+
+
     
 //     var tip_amount = document.getElementById('tipInput').value;
 //     document.getElementById('tipOutput').innerHTML = `${tip}%`;
