f6bde8e101bd9857e15392fcde8b2301672e9f39
vm9036 <vishalmakwana9714@gmail.com>
2023-03-24 17:42:48 +0530

    update tip calulator

--- all-task/js/tip_calculator.js
+++ all-task/js/tip_calculator.js
@@ -1,18 +1,19 @@
-function tip_calculator() {
+function tipCalculator() {
 
     let totalbillAmount = 0;
-
-    let bill_amount = Number(document.getElementById("amount").value);
+    let billAmount = Number(document.getElementById("billAmount").value);
     let interestAmount = Number(document.getElementById("tipPercentage").value);
+    let gstAmountValue = Number(document.getElementById("gstAmount").value);
 
-    let interest = (bill_amount * interestAmount) / 100;
-    document.getElementById("tip_amount").value = interest;
+    let interest = (billAmount * interestAmount) / 100;
+    document.getElementById("amount").value = interest;
 
-    totalbillAmount = bill_amount + interest;
-    document.getElementById("totalBill").value = totalbillAmount;
+    let gst = (billAmount * gstAmountValue) / 100;
+    document.getElementById("finalGst").value = gst;
 
+    totalbillAmount = interest + gst + billAmount;
+    document.getElementById("totalBillAmount").value = totalbillAmount;
 
-    
 //     var tip_amount = document.getElementById('tipInput').value;
 //     document.getElementById('tipOutput').innerHTML = `${tip}%`;
 //     var tipValue = bill * (tip/100);
