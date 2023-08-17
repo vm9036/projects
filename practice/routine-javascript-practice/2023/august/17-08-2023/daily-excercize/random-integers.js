1d36331c8fce60ec2612fd9fc8b45d37e8b5e2e4
vm9036 <107696677+vm9036@users.noreply.github.com>
2023-08-17 17:00:44 +0530

    update randome integers

--- practice/routine-javascript-practice/2023/august/17-08-2023/daily-excercize/random-integers.js
+++ practice/routine-javascript-practice/2023/august/17-08-2023/daily-excercize/random-integers.js
@@ -1,19 +1,17 @@
 function randomIntegers() {
-    // var options = this.options.get();
-    // var rangeStart = +(options["range-start"]);
-    // var rangeEnd = +(options["range-end"]);
-    // var howMany = +(options["count"]);
-
-    var rangeStart = Number(document.getElementById("range-start").value);
-    var rangeEnd = Number(document.getElementById("range-end").value);
-    var howMany = Number(document.getElementById("count").value);
+    var rangeStart = +(document.getElementById("range-start").value);
+    var rangeEnd = +(document.getElementById("range-end").value);
+    var howMany = +(document.getElementById("count").value);
     var outputStr = '';
     for (var i = 0; i < howMany; i++) {
         outputStr += generateRandomNumber(rangeStart, rangeEnd);
         outputStr += "\n";
-        document.getElementById("displayIntegers").value = outputStr;
     }
+    document.getElementById("displayIntegers").value = outputStr;
 }
+
 function generateRandomNumber(rangeStart, rangeEnd) {
     return Math.floor(Math.random() * (rangeEnd - rangeStart + 1)) + rangeStart;
 }
+
+randomIntegers();
