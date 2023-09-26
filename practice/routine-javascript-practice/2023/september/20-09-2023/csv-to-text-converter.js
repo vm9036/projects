cdee01f9f1c24d9b0fd222ade0723fa9b8aadd4f
vm9036 <107696677+vm9036@users.noreply.github.com>
2023-09-26 20:47:52 +0530

    updated csv to text converter

--- practice/routine-javascript-practice/2023/september/20-09-2023/csv-to-text-converter.js
+++ practice/routine-javascript-practice/2023/september/20-09-2023/csv-to-text-converter.js
@@ -1,29 +1,29 @@
-function csvToText(){
+function csvToText() {
 
-    var delimiter = document.getElementById("delimiter");
-    var comments = document.getElementById("comments");
-    var quoteChar = document.getElementById("quoteChar");
+    var delimiter = document.getElementById("char-delimiter").value;
+    var comments = document.getElementById("char-comments").value;
+    var quoteChar = document.getElementById("char-quote").value;
     var skipEmptyLines = document.getElementById("convert-skip-empty").checked;
-    console.log("skipEmptyLines :-> "+skipEmptyLines);
+    console.log("skipEmptyLines :-> " + skipEmptyLines);
     var convertHeaders = document.getElementById("convert-headers").checked;
-    console.log("convertHeaders :-> "+convertHeaders);
-    var o = document.getElementById("generatedCsvToTextInputTextArea").value;
+    console.log("convertHeaders :-> " + convertHeaders);
+    var o = document.getElementById("InputTextArea").value;
     s = {
-        delimiter: delimiter ||",",
+        delimiter: delimiter || ",",
         comments: comments || "#",
         quoteChar: quoteChar || '"',
         headers: !1,
         skipEmptyLines: skipEmptyLines
     }
-      , t = Papa.parse(o, s).data
-      , v = ""
-      , n = []
-      , f = 0;
+        , t = Papa.parse(o, s).data
+        , v = ""
+        , n = []
+        , f = 0;
     convertHeaders || (f = 1);
     for (var r = f; r < t.length; r++)
         for (var e = 0; e < t[r].length; e++)
             n[e] === void 0 && (n[e] = 0),
-            t[r][e].length > n[e] && (n[e] = t[r][e].length);
+                t[r][e].length > n[e] && (n[e] = t[r][e].length);
     for (var r = f; r < t.length; r++) {
         for (var i = t[r], e = 0; e < i.length; e++) {
             if (i[e].length < n[e])
@@ -34,11 +34,22 @@ function csvToText(){
         v += "\n"
     }
     let answer = v;
-    document.getElementById("generatedCsvToTextOutputTextArea").value = answer;
+    document.getElementById("OutputTextArea").value = answer;
 }
 
-function sampleBtn(){
-    const testStr = "fo,foo,fooo,foooo \n1,2,3,4";
-    document.getElementById("generatedCsvToTextInputTextArea").value = testStr;
+function sampleBtn() {
+    const testStr =
+`Col 1, Col 2, Col 3, Col 4
+Dog, Cat, Pony, Hamster
+
+Mango, Orange 
+Tiger, Lion, Elephant,#Giraffe`;
+    document.getElementById("InputTextArea").value = testStr;
     csvToText();
+}
+
+function csvToTextAuto() {
+    if (document.getElementById("isAuto").checked) {
+        csvToText();
+    }
 }
