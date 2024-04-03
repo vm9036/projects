efb6b438a96d5771b8e9f7c465aef0bf34dab42f
vm9036 <107696677+vm9036@users.noreply.github.com>
2024-04-03 15:46:39 +0530

    Updated Sortable Drag and Drop

---
+++ practice/routine-javascript-practice/2024/april/3-4-24/sortable-drag-and-drop/script.js
@@ -0,0 +1,47 @@
+const draggables = document.querySelectorAll('.draggable');
+const containers = document.querySelectorAll('.container');
+
+draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', () => {
+        draggable.classList.add('dragging');
+        // console.log("Drag Start...");
+    })
+
+    draggable.addEventListener('dragend', () => {
+        draggable.classList.remove('dragging');
+        // console.log("Drag End...");
+    })
+})
+
+containers.forEach(container => {
+    container.addEventListener('dragover', e => {
+        e.preventDefault();
+        const afterElement = getDragAfterElement(container, e.clientY);
+        const draggable = document.querySelector('.dragging');
+        // console.log(afterElement);
+        if(afterElement == null)
+        {
+            container.appendChild(draggable);
+        } else {
+            container.insertBefore(draggable, afterElement);
+        }
+        // console.log('drag over')
+    })
+})
+
+function getDragAfterElement(container, y) {
+    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect()
+        const offset = y - box.top - box.height / 2
+        // console.log(box);
+        // console.log(offset);
+
+        if (offset < 0 && offset > closest.offset) {
+            return { offset: offset, element: child }
+        } else {
+            return closest;
+        }
+    }, { offset: Number.NEGATIVE_INFINITY }).element
+};  
