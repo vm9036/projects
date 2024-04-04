cac679f7d75642c614eb62f8db7d105a3145f746
vm9036 <107696677+vm9036@users.noreply.github.com>
2024-04-04 10:38:00 +0530

    Updared Speed typing game

--- practice/routine-javascript-practice/2024/april/2-4-24/speed-typing-game/index.js
+++ practice/routine-javascript-practice/2024/april/2-4-24/speed-typing-game/index.js
@@ -1,63 +1,126 @@
-const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
+const RANDOM_QUOTE_API_URL = 'https://type.fit/api/quotes'
 const quoteDisplayElement = document.getElementById('quoteDisplay')
 const quoteInputElement = document.getElementById('quoteInput')
 const timerElement = document.getElementById('timer')
-
 
 quoteInputElement.addEventListener('input', () => {
     const arrayQuote = quoteDisplayElement.querySelectorAll('span')
     const arrayValue = quoteInputElement.value.split('')
 
-    let correct = true;
+    let correct = true
     arrayQuote.forEach((characterSpan, index) => {
         const character = arrayValue[index]
-        if(character == null) {
+        if (character == null) {
             characterSpan.classList.remove('correct')
             characterSpan.classList.remove('incorrect')
             correct = false
-        }
-        else if(character === characterSpan.innerText){
+        } else if (character === characterSpan.innerText) {
             characterSpan.classList.add('correct')
             characterSpan.classList.remove('incorrect')
         } else {
-             characterSpan.classList.remove('correct')
+            characterSpan.classList.remove('correct')
             characterSpan.classList.add('incorrect')
             correct = false
         }
     })
 
-    if(correct) renderNewQuote()
+    if (correct) renderNewQuote()
 })
 
 function getRandomQuote() {
     return fetch(RANDOM_QUOTE_API_URL)
         .then(response => response.json())
-        .then(data => data.content)
+        .then(data => data[Math.floor(Math.random() * data.length)].text) // Select a random quote
 }
 
 async function renderNewQuote() {
-    const quote = await getRandomQuote();
-    quoteDisplayElement.innerHTML = ''
+    const quote = await getRandomQuote()
+    quoteDisplayElement.innerHTML = ' '
     quote.split('').forEach(character => {
-        const charcterSpan = document.createElement('span')
-        charcterSpan.innerText = character;
-        quoteDisplayElement.appendChild(charcterSpan)
-    });
+        const characterSpan = document.createElement('span')
+        characterSpan.innerText = character
+        quoteDisplayElement.appendChild(characterSpan)
+    })
     quoteInputElement.value = null
     startTimer()
 }
 
-let startTime 
+let startTime
 function startTimer() {
- timerElement.innerText = 0;
- startTime = new Date()
- setInterval(() => {
-    timerElement.innerText = getTimerTime()
- }, 1000);
+    timerElement.innerText = 0
+    startTime = new Date()
+    setInterval(() => {
+        timer.innerText = getTimerTime()
+    }, 1000)
 }
 
 function getTimerTime() {
-  return  Math.floor((new Date() - startTime) / 1000)
+    return Math.floor((new Date() - startTime) / 1000)
 }
 
-renderNewQuote();
+renderNewQuote()
+
+
+// const RANDOM_QUOTE_API_URL = 'https://type.fit/api/quotes'
+// const quoteDisplayElement = document.getElementById('quoteDisplay')
+// const quoteInputElement = document.getElementById('quoteInput')
+// const timerElement = document.getElementById('timer')
+
+
+// quoteInputElement.addEventListener('input', () => {
+//     const arrayQuote = quoteDisplayElement.querySelectorAll('span')
+//     const arrayValue = quoteInputElement.value.split('')
+
+//     let correct = true;
+//     arrayQuote.forEach((characterSpan, index) => {
+//         const character = arrayValue[index]
+//         if(character == null) {
+//             characterSpan.classList.remove('correct')
+//             characterSpan.classList.remove('incorrect')
+//             correct = false
+//         }
+//         else if(character === characterSpan.innerText){
+//             characterSpan.classList.add('correct')
+//             characterSpan.classList.remove('incorrect')
+//         } else {
+//              characterSpan.classList.remove('correct')
+//             characterSpan.classList.add('incorrect')
+//             correct = false
+//         }
+//     })
+
+//     if(correct) renderNewQuote()
+// })
+
+// function getRandomQuote() {
+//     return fetch(RANDOM_QUOTE_API_URL)
+//         .then(response => response.json())
+//         .then(data => data.content)
+// }
+
+// async function renderNewQuote() {
+//     const quote = await getRandomQuote();
+//     quoteDisplayElement.innerHTML = ''
+//     quote.split('').forEach(character => {
+//         const charcterSpan = document.createElement('span')
+//         charcterSpan.innerText = character;
+//         quoteDisplayElement.appendChild(charcterSpan)
+//     });
+//     quoteInputElement.value = null
+//     startTimer()
+// }
+
+// let startTime 
+// function startTimer() {
+//  timerElement.innerText = 0;
+//  startTime = new Date()
+//  setInterval(() => {
+//     timerElement.innerText = getTimerTime()
+//  }, 1000);
+// }
+
+// function getTimerTime() {
+//   return  Math.floor((new Date() - startTime) / 1000)
+// }
+
+// renderNewQuote();
